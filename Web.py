e271cfc6a2d6b3b41f5d3462635710a02adc411a
regaarz <regaarzulaakbar93@gmail.com>
2022-09-09 17:55:53 +0700

    new

--- Web.py
+++ Web.py
@@ -12,6 +12,10 @@ inputSuhu = db["input temperatur"]
 app = Flask(__name__)
 
 @app.route('/')
+def front():
+    return render_template('front.html')
+
+@app.route('/index')
 def index():
     return render_template ('index.html')
 
